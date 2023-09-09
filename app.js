f014683965a4b8edf2fe3a6d1ef0e2d14dda771f
AishaKhalfan <aishakhalfan45@gmail.com>
2023-09-10 00:53:21 +0300

    UTC TIME

--- app.js
+++ app.js
@@ -24,14 +24,33 @@ app.get('/api', (req, res) => {
   }
 
   const now = moment().utc();
-  const utcTime = now.format('YYYY-MM-DDTHH:mm:ssZ');
+  //const utcTime = now.format('YYYY-MM-DDTHH:mm:ssZ');
+  const utcTime = now.toISOString();
+  const currentDay = now.format('dddd');
 
-  const githubFileUrl = `"https://github.com/AishaKhalfan/HNGx-INTERNSHIP/blob/main/BackendStage1/app.js`;
-  const githubRepoUrl = `"https://github.com/AishaKhalfan/HNGx-INTERNSHIP/`;
+  // Check if the UTC time is within a +/-2 minute window
+  const currentTime = now.format('HH:mm'); // Format as hours and minutes
+  //const isValidTime = moment(currentTime, 'HH:mm').isBetween('08:00', '16:00');
+  // Validate UTC time within +/- 2 minutes
+  const isValidTime = now.isBetween(
+    moment().subtract(2, 'minutes'),
+    moment().add(2, 'minutes')
+  );
+
+  if (!isValidTime) {
+    res.status(500);
+    res.json({
+      error: 'UTC time is not within the allowed window',
+    });
+    return;
+  }
+
+  const githubFileUrl = "https://github.com/AishaKhalfan/HNGx-INTERNSHIP/blob/main/app.js";
+  const githubRepoUrl = "https://github.com/AishaKhalfan/HNGx-INTERNSHIP/";
 
   const response = {
     slack_name: slackName,
-    current_day: moment().format('dddd'),
+    current_day: currentDay,
     utc_time: utcTime,
     track: track,
     github_file_url: githubFileUrl,
